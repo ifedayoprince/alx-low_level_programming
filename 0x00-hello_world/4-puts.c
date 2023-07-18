b9d0141e3170484585675667b06bcc7d2e439e41
Ifedayo Prince Oni <ifedayoprince@gmail.com>
2023-07-18 02:02:13 +0100

    Initial Commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Author: Ifedayo Prince <ifedayoprince@gmail.com>
+ */
+
+#include <stdio.h>
+.
+/**
+ * main - Entry function designed to Output "Programming is like building a multilingual
+ *                puzzle", followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * File: 5-printf.c
+ * Author: Ifedayo P Oni <ifedayoprince@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "the text with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
