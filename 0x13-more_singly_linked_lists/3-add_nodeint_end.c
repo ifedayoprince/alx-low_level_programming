ba68297d7cb6c9c6dc977ca3230b31792cb4b86e
ifedayoprince <ifedayoprince@gmail.com>
2023-10-25 00:43:30 +0100

    fix: rewrite 'add_nodeint_end' to erase the errors by betty

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,12 +17,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new, **temp = head;
-
-	while (*temp)
-	{
-		temp = &((*temp)->next);
-	}
+	listint_t *new, *temp;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
@@ -31,7 +26,17 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 
-	temp = &new;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
 
 	return (*head);
 }
